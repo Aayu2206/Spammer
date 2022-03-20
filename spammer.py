7c7cb982d6b67ce11811c6fe8831d24a7c144314
Aayush Saini <92023704+Aayu2206@users.noreply.github.com>
2022-03-20 12:14:20 +0530

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,11 @@
+import pyautogui as pg
+import time
+
+#Change the time delay
+time.sleep(1)
+
+for i in range(10):
+    #Enter the text you want to spam
+    pg.write('')
+    #presses enter after sending a message
+    pg.press('Enter')
